c2e44fdd81da9061c4bb1483e8bdc8fbbb4dd315
IT24100258 <it24100258@my.sliit.lk>
2025-09-02 15:02:32 +0530

    Lab Sheet 06 submission

---
+++ Lab Sheet 06/IT24100258.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\Tuf\\Desktop\\SLIIT\\Y2S1\\P&S\\WEEK_7\\Lab 06-20250902")
+
+#1
+#I. n = 50, p = 0.85
+
+#II
+pbinom(47, 50, 0.85)
+
+#2
+#I. number of calls per hour
+
+#II. Poisson Distribution -> lambda=12
+
+#III.
+dpois(15, 5)
